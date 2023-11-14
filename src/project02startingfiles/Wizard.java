b7416ea2fe3dccdb61b9b2bb7abc8680be2a24ef
Allison Verhulst <122564024+verhulsta532@users.noreply.github.com>
2023-11-14 16:44:07 -0600

    Create Wizard

---
+++ src/project02startingfiles/Wizard.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project02startingfiles;
+
+/**
+ *
+ * @author s548114
+ */
+public class Wizard extends Player {
+
+    private String specialMove;
+
+    public Wizard(int health, int score) {
+        super(health, score);
+    }
+
+    @Override
+    public String getSpecialMove() {
+        return "Cast a Spell";
+    }
+
+    @Override
+    public void useSpecialMove() {
+        System.out.println("The Wizard casts a spell!");
+    }
+
+    @Override
+    public String toString() {
+        return "Wizard Status:\nHealth: " + getHealth() + "\nScore: " + getScore() + "\nThe Wizard's Special move is " + specialMove;
+    }
+
+}
