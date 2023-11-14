46290d54cbe2c3b5bf880b95691c2ee657bf2ce2
Allison Verhulst <122564024+verhulsta532@users.noreply.github.com>
2023-11-14 16:31:55 -0600

    Created healter

---
+++ src/project02startingfiles/Healer.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project02startingfiles;
+
+/**
+ *
+ * @author s548114
+ */
+public class Healer extends Player {
+
+    private String specialMove;
+
+    public Healer(int health, int score) {
+        super(health, score);
+    }
+
+    @Override
+    public String getSpecialMove() {
+        return "Slash with Sword";
+    }
+
+    @Override
+    public void useSpecialMove() {
+        System.out.println("The healer slashes with the sword!");
+    }
+
+    @Override
+    public String toString() {
+        return "Healer Status:\nHealth: " + getHealth() + "\nScore: " + getScore() + "\nThe Healer's Special move is " + specialMove;
+    }
+
+}
