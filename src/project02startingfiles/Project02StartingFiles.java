3dd3766b742a17f447eb927922d0adf8af64c450
s543153 <s543153@nwmissouri.edu>
2023-11-14 18:15:59 -0600

    Sal and Allison completed together in library!

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -5,7 +5,7 @@
  */
 package project02startingfiles;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *
@@ -20,6 +20,7 @@ public class Project02StartingFiles {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
         String cChoice;
+        Random rand = new Random();
 
         System.out.println("Welcome to JavaQuest!");
         System.out.println("Stay alive and increase your score!");
@@ -44,7 +45,78 @@ public class Project02StartingFiles {
                 System.out.println("Invalid choice. Exiting the game");
                 return;
         }
-        System.out.println(player);
+        System.out.println("What would you like to do?");
+        System.out.println("{?}Status Report || {n}{s}{e}{w} move one space north, south, east, or west || {q} quit");
+        cChoice = input.nextLine();
+        while (cChoice != "q") {
+            if (cChoice == "?") {
+                System.out.println(player);
+            }
+        }
+    }
+
+    private static void move(Player player, Random random) {
+        int randomEvent = random.nextInt(5);
+
+        if (randomEvent == 0) {
+            System.out.println("Nice trees around");
+            player.setScore(player.getScore() + 1);
+        } else if (randomEvent == 1) {
+            System.out.println("Potty break");
+            player.setScore(player.getScore() + 1);
+        } else if (randomEvent == 2) {
+
+        }
+    }
+
+    private static void attack(Player player, Random random) {
+        int randomEnemy = random.nextInt(3);
+        int chance = random.nextInt(5);
+        int run = random.nextInt(2);
+        Scanner input = new Scanner(System.in);
+        String choice;
+
+        if (randomEnemy == 0) {
+            System.out.println("A zombie has appeared!!!");
+        } else if (randomEnemy == 1) {
+            System.out.println("A bandit has snuck up on you!");
+        } else if (randomEnemy == 2) {
+            System.out.println("A lobbyist are approaching you :( ");
+        }
+        System.out.println("How would you like to handle this?");
+        System.out.println("{s}Special Move || {r}Run!");
+        choice = input.nextLine();
+        if (choice == "s") {
+            System.out.println("Prepare for battle!");
+            System.out.println("Choose anyletter the ENTER to continue");
+            choice = input.nextLine();
+            if (chance == 0 || chance == 1 || chance == 2) {
+                player.useSpecialMove();
+                System.out.println("Player wins increase score by 2");
+                player.setScore(player.getScore() + 2);
+                System.out.println(player);
+
+            } else {
+                player.useSpecialMove();
+                System.out.println("You missed... 1 health point deducted");
+                player.setHealth(player.getHealth() - 1);
+                System.out.println(player);
+
+            }
+            if (choice == "r") {
+                System.out.println("You attempted to run away!");
+                if (run == 0) {
+                    System.out.println("Succesful you got away! + 1 score");
+                    player.setScore(player.getScore() + 1);
+
+                } else if (run == 1) {
+                    System.out.println("You were not succesful");
+                }
+
+            }
+
+        }
 
     }
+
 }
